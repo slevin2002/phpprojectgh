4925f9fdc6b02bd78d92685dcee59d4249d8073f
slevin2002 <slevinvarghees@gmail.com>
2022-12-14 10:46:33 +0530

    new example
    new example from https://www.geeksforgeeks.org/how-to-use-php-in-html/#:~:text=You%20can%20add%20PHP%20tags,with%20the%20PHP%20starts%20tag%20%3C%3F

---
+++ example1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<style>
+		h1 {
+			color: green;
+		}
+	</style>
+</head>
+
+<body>
+	<center>
+		<h1>Welcome To GFG</h1>
+
+		<h2>
+			<?php
+			echo "This is PHP code inside html"
+			?>
+		</h2>
+	</center>
+</body>
+
+</html>
